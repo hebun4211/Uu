21667fd86500fb870f80553202b9df91b6462c58
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-05-19 22:37:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ BOT_TOKEN = os.getenv("7269425990:AAElkYfpjGBp0rI2sLtRb83lKLIK7IMhrYk")
 
 OWNER = os.getenv("@Arayanlarbulamadi")
 
-
+BOT_USERNAME= os.getenv("@ilistinmusicbot")
 
 # pyrogram client
 
